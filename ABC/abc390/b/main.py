0a5f61f981be2f21f1ba732ff77638e129bb48f5
Ryo <ryo.na.na.ryo.na@gmail.com>
2025-01-26 01:12:47 +0900

    fix: ABC390-B

--- ABC/abc390/b/main.py
+++ ABC/abc390/b/main.py
@@ -5,8 +5,7 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
-    # from decimal import Decimal, getcontext
-    # getcontext().prec = 100000
+    from fractions import Fraction
     
     n = int(input())
     a = list(map(int, input().split()))
@@ -16,10 +15,9 @@ def main():
         return
         
     
-    gosa = 1e-9
-    r = a[1] / a[0]
+    r = Fraction(a[1], a[0])
     for i in range(1, n):
-        if abs(a[i - 1] * r - a[i]) > gosa:
+        if a[i - 1] * r != a[i]:
             print("No")
             return
     print("Yes")
