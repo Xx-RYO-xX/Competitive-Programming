7e097c9618e3af350d8751b2bc4d45b9d8e58955
Ryo <ryo.na.na.ryo.na@gmail.com>
2025-02-22 20:29:00 +0900

    solve: DP-d, e

--- DP/dp/d/main.py
+++ DP/dp/d/main.py
@@ -5,8 +5,17 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
+    n, W = map(int, input().split())
+    dp = [[0]*(W+1) for i in range(n+1)]
+    for i in range(n):
+        w, v = map(int, input().split())
+        for j in range(W+1):
+            if 0 <= j-w:
+                dp[i+1][j] = max(dp[i][j], dp[i][j-w]+v)
+            dp[i+1][j] = max(dp[i+1][j], dp[i][j])
     
-
+    # print(dp)
+    print(dp[n][W])
 
 if __name__ == '__main__':
     main()
--- DP/dp/e/main.py
+++ DP/dp/e/main.py
@@ -5,8 +5,19 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
-    
+    VALUE = 10**5+1
+    n, W = map(int, input().split())
+    dp = [[float("inf")]*VALUE for i in range(n+1)]
+    dp[0][0] = 0
+    for i in range(n):
+        w, v = map(int, input().split())
+        for j in range(VALUE):
+            dp[i+1][j] = dp[i][j] 
+            if j >= v:
+                dp[i+1][j] = min(dp[i+1][j], dp[i][j-v]+w)
 
+        # print(dp[i])
+    print(max(j for j in range(VALUE) if dp[n][j] <= W))
 
 if __name__ == '__main__':
     main()
