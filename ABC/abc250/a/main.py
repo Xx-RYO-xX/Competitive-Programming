512f8c0de8d8f4c38158bfd7a2b68bb4e1fb2ad8
Ryo <ryo.na.na.ryo.na@gmail.com>
2025-03-01 16:53:52 +0900

    solve: ABC250-A

---
+++ ABC/abc250/a/main.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def input():return sys.stdin.readline().rstrip()
+
+
+def main():
+    h, w = map(int, input().split())
+    r, c = map(int, input().split())
+
+    ans = 0
+    for i in range(1, h+1):
+        for j in range(1, w+1):
+            if abs(i-r)+abs(j-c) == 1:
+                ans += 1
+    
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
