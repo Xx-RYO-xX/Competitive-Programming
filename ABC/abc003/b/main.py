8f443e9e26a96afa66c153cf7d9da44f43c4bdc2
Ryo <ryo.na.na.ryo.na@gmail.com>
2025-03-14 13:11:43 +0900

    solve: ABC003-B, C

--- ABC/abc003/b/main.py
+++ ABC/abc003/b/main.py
@@ -5,7 +5,23 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
+    s = input()
+    t = input()
 
+    atcoder = set({"a", "t", "c", "o", "d", "e", "r", "@"})
+    
+    for i in range(len(s)):
+        if s[i] == "@" and t[i] not in atcoder:
+            print("You will lose")
+            return
+        elif t[i] == "@" and s[i] not in atcoder:
+            print("You will lose")
+            return
+        elif s[i] != t[i] and (s[i] != "@" and t[i] != "@"):
+            print("You will lose")
+            return
+    
+    print("You can win")
 
 if __name__ == '__main__':
     main()
--- ABC/abc003/c/main.py
+++ ABC/abc003/c/main.py
@@ -5,7 +5,14 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
+    n, k = map(int, input().split())
+    r = sorted(map(int, input().split()))
 
+    ans = 0.0
+    for i in range(n-k, n):
+        ans = (r[i]+ans)/2
+    
+    print(ans)
 
 if __name__ == '__main__':
     main()
