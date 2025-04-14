532d72b2cd430717ea3adea4fa19a700d01754a4
Ryo <ryo.na.na.ryo.na@gmail.com>
2025-04-14 14:16:55 +0900

    solve: ABC213-C

--- ABC/abc213/c/main.py
+++ ABC/abc213/c/main.py
@@ -5,12 +5,25 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
+    from bisect import bisect_left
     h, w, n = map(int, input().split())
-    a, b = [], []
+    a, b = set(), set()
+    
+    ab = []
     for _ in range(n):
         at, bt = map(int, input().split())
-        a.append(at)
-        b.append(bt)
+        a.add(at)
+        b.add(bt)
+        ab.append((at, bt))
+    a_lst = sorted(a)
+    b_lst = sorted(b)
+
+    for i in range(n):
+        a, b = ab[i]
+        a_bis = bisect_left(a_lst, a)
+        b_bis = bisect_left(b_lst, b)
+        print(a-(a_lst[a_bis]-a_bis)+1, b-(b_lst[b_bis]-b_bis)+1)
+    
     
 
 
--- ABC/abc213/d/main.py
+++ ABC/abc213/d/main.py
@@ -4,8 +4,42 @@ import sys
 def input():return sys.stdin.readline().rstrip()
 
 
+
+
 def main():
+    from collections import defaultdict
+    from sortedcontainers import SortedList
+    n = int(input())
+    g_lst = defaultdict(SortedList)
+    for _ in range(n-1):
+        a, b = map(int, input().split())
+        g_lst[a].add(b)
+        g_lst[b].add(a)
+    print(g_lst)
     
+    def dfs(pos, graph_lst, visited, path, goal):
+        global finished
+        if finished:
+            return
+
+        path.append(pos)
+        visited[pos] = True
+        if pos == goal:
+            finished = True
+            return
+
+        for i in graph_lst[pos]:
+            if not visited[i]:
+                dfs(i, graph_lst, visited, path, goal)
+                if finished:
+                    return
+
+        path.pop()
+    
+    print()
+    dfs(1, )
+
+
 
 
 if __name__ == '__main__':
