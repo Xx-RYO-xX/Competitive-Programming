60f32d31f1e9d83beb55cc80281812d0adda0fa2
Ryo <ryo.na.na.ryo.na@gmail.com>
2025-05-25 13:07:41 +0900

    solve: ABC243-C

--- ABC/abc243/c/main.py
+++ ABC/abc243/c/main.py
@@ -8,24 +8,26 @@ def main():
     from collections import defaultdict
     
     n = int(input())
-    y_to_xi = defaultdict(list)
-    yy = set()
+    y_to_x = defaultdict(list)
+    y_ = set()
     for i in range(n):
         x, y = map(int, input().split())
-        y_to_xi[y].append((x, i))
-        yy.add(y)
+        y_to_x[y].append((x, i))
+        y_.add(y)
     s = input()
     
-    for y in yy:
-        if len(y_to_xi[y]) >= 2:
-            y_to_xi[y].sort()
+    for y in y_:
+        if len(y_to_x[y]) >= 2:
+            y_to_x[y].sort()
             right_cond = False
-            for x, i in y_to_xi[y]:
+            for x, i in y_to_x[y]:
                 if s[i] == "R":
                     right_cond = True
                 if right_cond and s[i] == "L":
                     print("Yes")
                     return
+    
+    
     
     print("No")
     
