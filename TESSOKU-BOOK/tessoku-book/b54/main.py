bfb0483538948c2cd9fce445b3c37291c04c9969
Ryo <ryo.na.na.ryo.na@gmail.com>
2025-05-25 13:39:52 +0900

    solve: TESSOKU-BOOK-b54

--- TESSOKU-BOOK/tessoku-book/b54/main.py
+++ TESSOKU-BOOK/tessoku-book/b54/main.py
@@ -5,8 +5,17 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
-    
+    import math
+    from collections import Counter
+    n = int(input())
+    a = [int(input()) for _ in range(n)]
 
+    ans = 0
+    cnt = Counter(a)
+    for c in cnt:
+        ans += math.comb(cnt[c], 2)
+    
+    print(ans)
 
 if __name__ == '__main__':
     main()
