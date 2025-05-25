fceb0fba65bb477481b6e0438ee8b6dfbc522166
Ryo <ryo.na.na.ryo.na@gmail.com>
2025-05-25 13:39:31 +0900

    solve: TESSOKU-BOOK-a54

--- TESSOKU-BOOK/tessoku-book/a54/main.py
+++ TESSOKU-BOOK/tessoku-book/a54/main.py
@@ -5,7 +5,16 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
-    
+    q = int(input())
+    seiseki = dict()
+    for _ in range(q):
+        query = input()
+        if query[0] == "1":
+            num, x, y = query.split()
+            seiseki[x] = y
+        else:
+            num, x = query.split()
+            print(seiseki[x])
 
 
 if __name__ == '__main__':
