3dc090ddf867d06a5146b638a9b22bc546dbae5d
ryo-nakagawa1012 <ryo.na.na.ryo.na@gmail.com>
2025-07-05 20:55:51 +0900

    solve: TESSOKU-BOOK-a64, b64

--- TESSOKU-BOOK/tessoku-book/a64/main.py
+++ TESSOKU-BOOK/tessoku-book/a64/main.py
@@ -5,20 +5,38 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
-    import networkx as nx
+    from collections import defaultdict
+    import heapq
     
     n, m = map(int, input().split())
-    graph = nx.Graph()
-    graph.add_nodes_from(range(1, n+1))
-    egdes = []
+    g = defaultdict(set)
     for _ in range(m):
         a, b, c = map(int, input().split())
-        egdes.append((a, b, c))
-    graph.add_weighted_edges_from(egdes)
+        g[a].add((b, c))
+        g[b].add((a, c))
     
-    distances = nx.single_source_dijkstra_path_length(G=graph, source=1)
+    inf = float("inf")
+    cur = defaultdict(lambda: inf)
+    kakutei = defaultdict(lambda: False)
+    q = []
+    cur[1] = 0
+    heapq.heappush(q, (cur[1], 1))
+    while q:
+        pos = heapq.heappop(q)[1]
+        if kakutei[pos]:
+            continue
+        kakutei[pos]= True
+        for nex in g[pos]:
+            nex_pos, nex_dist = nex
+            if cur[nex_pos] > cur[pos]+nex_dist:
+                cur[nex_pos] = cur[pos]+nex_dist
+                heapq.heappush(q, (cur[nex_pos], nex_pos))
+    
     for i in range(1, n+1):
-        print(distances.get(i, "-1"))
+        if cur[i] == inf:
+            print(-1)
+        else:
+            print(cur[i])
 
 
 if __name__ == '__main__':
--- TESSOKU-BOOK/tessoku-book/b64/main.py
+++ TESSOKU-BOOK/tessoku-book/b64/main.py
@@ -5,18 +5,45 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
-    import networkx as nx
+    from collections import defaultdict
+    import heapq
     
     n, m = map(int, input().split())
-    graph = nx.Graph()
-    graph.add_nodes_from(range(1, n+1))
-    edges = list()
+    g = defaultdict(set)
     for _ in range(m):
         a, b, c = map(int, input().split())
-        edges.append((a, b, c))
-    graph.add_weighted_edges_from(edges)
-    ans = nx.dijkstra_path(G=graph, source=1, target=n)
-    print(*ans)
+        g[a].add((b, c))
+        g[b].add((a, c))
+
+    inf = float("inf")
+    cur = defaultdict(lambda: inf)
+    kakutei = defaultdict(lambda: False)
+    prev = defaultdict(lambda:-1)
+    q = []
+    cur[1] = 0
+    heapq.heappush(q, (cur[1], 1))
+    while q:
+        pos = heapq.heappop(q)[1]
+        if kakutei[pos]:
+            continue
+        kakutei[pos]= True
+        for nex in g[pos]:
+            nex_pos, nex_dist = nex
+            if cur[nex_pos] > cur[pos]+nex_dist:
+                cur[nex_pos] = cur[pos]+nex_dist
+                heapq.heappush(q, (cur[nex_pos], nex_pos))
+                prev[nex_pos] = pos
+    
+    if cur[n] == inf:
+        print(-1)
+        return
+
+    path = []
+    node = n
+    while node != -1:
+        path.append(node)
+        node = prev[node]
+    print(*path[::-1])
 
 if __name__ == '__main__':
     main()
