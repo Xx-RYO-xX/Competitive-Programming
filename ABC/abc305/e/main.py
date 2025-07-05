61a6eac014fc85b32be412ecc0b293af8dc7dc87
ryo-nakagawa1012 <ryo.na.na.ryo.na@gmail.com>
2025-07-05 20:55:11 +0900

    challenge: ABC305-E

--- ABC/abc305/e/main.py
+++ ABC/abc305/e/main.py
@@ -5,7 +5,35 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
+    from collections import defaultdict
+    sys.setrecursionlimit(10**9)
+    n, m, k = map(int, input().split())
+    g = defaultdict(list)
+    for _ in range(m):
+        a, b = map(int, input().split())
+        g[a].append(b)
+        g[b].append(a)
+    
+    visited = [False]+[False]*n        
+    for _ in range(k):
+        p, h = map(int, input().split())
+        keibi = defaultdict(lambda: False)
+        def dfs(pos, h, depth=0):
+            visited[pos] = True
+            keibi[pos] = True
+            if h <= depth:
+                return 
+            for nex in g[pos]:
+                if not keibi[nex]:
+                    dfs(nex, h, depth=depth+1)
+        dfs(p, h)
 
+    ans = []
+    for i in range(1, n+1):
+        if visited[i]:
+            ans.append(i)
+    print(len(ans))
+    print(*ans)
 
 if __name__ == '__main__':
     main()
