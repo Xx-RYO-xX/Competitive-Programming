25bb0bbebe3545284bbaff6e34c7be3f8b2cb34d
ryo-nakagawa1012 <ryo.na.na.ryo.na@gmail.com>
2025-07-12 20:53:45 +0900

    solve: ABC205-D

--- ABC/abc205/d/main.py
+++ ABC/abc205/d/main.py
@@ -5,11 +5,18 @@ def input():return sys.stdin.readline().rstrip()
 
 
 def main():
-    from bisect import bisect_right
+    from bisect import bisect_left
+
     n, q = map(int, input().split())
-    a = list(map(int, input().split()))
+    a = sorted(set(map(int, input().split())))
+    m = [a[i] - (i+1) for i in range(len(a))]
     for _ in range(q):
         k = int(input())
+        pos = bisect_left(m, k)
+        if pos == 0:
+            print(k)
+        else:
+            print(a[pos-1] + (k-m[pos-1]))
         
 
 
