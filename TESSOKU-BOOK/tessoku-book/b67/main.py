c6dc3d113e1af75d1885fe95f23f1130145eaba7
Ryo <ryo.na.na.ryo.na@gmail.com>
2025-08-11 12:49:43 +0900

    solve: TESSOKU-BOOK-a67, b67

--- TESSOKU-BOOK/tessoku-book/b67/main.py
+++ TESSOKU-BOOK/tessoku-book/b67/main.py
@@ -1,23 +1,79 @@
+from collections import defaultdict
 import sys 
 
 
 def input():return sys.stdin.readline().rstrip()
 
 
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+
+
+
+
 def main():
-    import networkx as nx
-    
     n, m = map(int, input().split())
-    graph = nx.Graph()
-    graph.add_nodes_from(range(1, n+1))
-    edges = []
+    hen = []
     for _ in range(m):
         a, b, c = map(int, input().split())
-        edges.append((a, b, c))
-
-    ans_tree = nx.maximum_flow(graph)
+        hen.append((a, b, c))
     
-    print(graph)
+    hen.sort(key=lambda x:x[2], reverse=True)
+    u = UnionFind(n+1)
+    ans = 0
+    for henn in hen:
+        a, b, c = henn
+        if not u.same(a, b):
+            u.union(a, b)
+            ans+=c
+    print(ans)
 
 
 if __name__ == '__main__':
