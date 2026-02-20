d4ed8b85d1bbbde4001b08c23a53b00dc71f30a5
Xx-RYO-xX <ryo.na.na.ryo.na@gmail.com>
2026-02-20 12:42:25 +0900

    solve: ABC247-D

--- ABC/abc247/d/main.py
+++ ABC/abc247/d/main.py
@@ -1,12 +1,31 @@
-import sys 
+import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
-    
+    from collections import deque
+
+    q = int(input())
+    tutu = deque([])
+    for _ in range(q):
+        query = list(map(int, input().split()))
+        if query[0] == 1:
+            que, x, c = map(int, query)
+            tutu.append([x, c])
+        else:
+            que, c = map(int, query)
+            ans = 0
+            while tutu[0][1] < c:
+                x, cc = tutu.popleft()
+                ans += x * cc
+                c -= cc
+            ans += tutu[0][0] * c
+            tutu[0][1] -= c
+            print(ans)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
