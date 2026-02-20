5f04f55db95cc4390b6ba93349abc0039b10fa61
Xx-RYO-xX <ryo.na.na.ryo.na@gmail.com>
2026-02-20 12:22:39 +0900

    solve: ABC250-B, C

--- ABC/abc250/b/main.py
+++ ABC/abc250/b/main.py
@@ -1,12 +1,31 @@
 import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
     n, a, b = map(int, input().split())
-    
+    one = ""
+    for i in range(n):
+        if i % 2 == 0:
+            one += "." * b
+        else:
+            one += "#" * b
+    two = ""
+    for i in range(n):
+        if i % 2 == 0:
+            two += "#" * b
+        else:
+            two += "." * b
 
-if __name__ == '__main__':
+    for i in range(a * n):
+        if (i // a) % 2 == 0:
+            print(one)
+        else:
+            print(two)
+
+
+if __name__ == "__main__":
     main()
--- ABC/abc250/c/main.py
+++ ABC/abc250/c/main.py
@@ -1,11 +1,32 @@
 import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
+    n, q = map(int, input().split())
+    a = [i for i in range(1, n + 1)]
+    num_to_idx = dict()
+    for idx, num in enumerate(a):
+        num_to_idx[num] = idx
+
+    for _ in range(q):
+        x = int(input())
+        left_num = x if num_to_idx[x] != n - 1 else a[-2]
+        right_num = a[num_to_idx[x] + 1] if num_to_idx[x] != n - 1 else a[-1]
+
+        left_idx = num_to_idx[left_num]
+        right_idx = left_idx + 1
+
+        a[left_idx], a[right_idx] = a[right_idx], a[left_idx]
+
+        num_to_idx[left_num] = right_idx
+        num_to_idx[right_num] = left_idx
+
+    print(*a)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
