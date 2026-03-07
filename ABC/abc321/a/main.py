06331bd2cbaa7fd632313272d41c115fcbdd6e68
Xx_RYO_xX <ryo.na.na.ryo.na@gmail.com>
2026-03-07 20:41:21 +0900

    solve: ABC321-A, C

--- ABC/abc321/a/main.py
+++ ABC/abc321/a/main.py
@@ -1,11 +1,18 @@
-import sys 
+import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
+    n = input()
+    for i in range(len(n) - 1):
+        if n[i] <= n[i + 1]:
+            print("No")
+            return
+    print("Yes")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
--- ABC/abc321/c/main.py
+++ ABC/abc321/c/main.py
@@ -1,12 +1,39 @@
-import sys 
+import sys
+import os
+import pickle
+
+CACHE_FILE = "cashe.pkl"
+
+def precompute():
+    nums =[]
+    for i in range(0, 9876543211):
+        si = str(i)
+        for ii in range(len(si) - 1):
+            if si[ii] <= si[ii + 1]:
+                break
+        else:
+            nums.append(i)
+    with open(CACHE_FILE, "wb") as f:
+        pickle.dump(nums, f)
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
-    
+    k = int(input())
+
+    with open(CACHE_FILE, "rb") as f:
+        nums = pickle.load(f)
+
+    print(nums[k])
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    if (len(sys.argv) > 1 and sys.argv[-1] == 'ONLINE_JUDGE'):
+        precompute()
+    else:
+        if not os.path.exists(CACHE_FILE):
+            precompute()
+        sys.exit(main())
