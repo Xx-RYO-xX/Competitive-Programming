de86043b95c0ac704331bd077bb4a4bf14e32482
Xx_RYO_xX <ryo.na.na.ryo.na@gmail.com>
2026-05-05 21:43:08 +0900

    solve: ABC272-C

--- ABC/abc272/c/main.py
+++ ABC/abc272/c/main.py
@@ -1,26 +1,37 @@
-import sys 
+import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
-    n, = int(input())
+    n = int(input())
     a = list(map(int, input().split()))
-    
+
     even = []
     odd = []
-    
+
     for i in range(n):
         if a[i] % 2 == 0:
             even.append(a[i])
         else:
             odd.append(a[i])
-    
+
     even.sort(), odd.sort()
 
-    
+    if len(even) == len(odd) == 1:
+        print(-1)
+        return
+
+    ans = []
+    if len(even) >= 2:
+        ans.append(even[-1] + even[-2])
+    if len(odd) >= 2:
+        ans.append(odd[-1] + odd[-2])
+
+    print(max(ans))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
