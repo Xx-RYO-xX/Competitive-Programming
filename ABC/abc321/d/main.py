3d9c668ea59b1740881cf6ee9e6ca8c2af383b75
Xx_RYO_xX <ryo.na.na.ryo.na@gmail.com>
2026-05-08 14:35:57 +0900

    solve: ABC321-D

--- ABC/abc321/d/main.py
+++ ABC/abc321/d/main.py
@@ -1,12 +1,37 @@
-import sys 
+import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
-    
+    from itertools import accumulate
+
+    n, m, p = map(int, input().split())
+    a = sorted(map(int, input().split()))
+    b = sorted(map(int, input().split()))
+
+    b_accm = list(accumulate(b))
+
+    ans = 0
+    for A in a:
+        left = 0
+        right = m
+
+        while left < right:
+            mid = (left + right) // 2
+            if A + b[mid] < p:
+                left = mid + 1
+            else:
+                right = mid
+        # print(left)
+        ans += p * (m - left)
+        # print(ans)
+        ans += A * left + b_accm[left - 1] if left != 0 else 0
+        # print(ans)
+    print(ans)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
