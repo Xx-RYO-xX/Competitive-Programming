2eea92cee8c8b762f33c201fd7aa4a434aedf421
Xx_RYO_xX <ryo.na.na.ryo.na@gmail.com>
2026-05-09 20:20:25 +0900

    solve: ABC298-D, F

--- ABC/abc298/d/main.py
+++ ABC/abc298/d/main.py
@@ -1,32 +1,40 @@
 import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
     MOD = 998244353
-    from collections import deque 
+
     q = int(input())
     s = 1
-    s_str = deque([1])
+    s_str = [1]
+    slen = 1
+    sentou_idx = 0
     for _ in range(q):
         query = input()
         if query[0] == "1":
-            Q, x =  map(int, query.split())
+            Q, x = map(int, query.split())
             s *= 10
             s += x
+            s %= MOD
             s_str.append(x)
+            slen += 1
         if query[0] == "2":
-            sentou = int(s_str.popleft())
-            minus = sentou * 10**len(s_str)
+            sentou = s_str[sentou_idx]
+            sentou_idx += 1
+            slen -= 1
+            minus = (sentou * pow(10, slen, MOD)) % MOD
             # print(sentou, minus)
             s -= minus
-        
+            s %= MOD
+
         if query[0] == "3":
-        # print(s_str)
-            print(s%MOD)
+            # print(s_str)
+            print(s % MOD)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- ABC/abc298/f/main.py
+++ ABC/abc298/f/main.py
@@ -1,11 +1,38 @@
 import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
+    from collections import defaultdict
+
+    n = int(input())
+    rr = defaultdict(int)
+    cc = defaultdict(int)
+    masu = dict()
+    for _ in range(n):
+        r, c, x = map(int, input().split())
+        rr[r] += x
+        cc[c] += x
+        masu[(r, c)] = x
+    # print(rr)
+    # print(cc)
+    rr = sorted([(val, key) for key, val in rr.items()], reverse=True)
+    cc = sorted([(val, key) for key, val in cc.items()], reverse=True)
+
+    ans = 0
+    for rval, rkey in rr:
+        for cval, ckey in cc:
+            if (rkey, ckey) not in masu:
+                ans = max(ans, rval + cval)
+                break
+            else:
+                ans = max(ans, rval + cval - masu[(rkey, ckey)])
+
+    print(ans)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
