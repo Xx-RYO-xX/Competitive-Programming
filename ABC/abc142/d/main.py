420095333c0af14cb04c6e918838b03c6f62639f
Xx_RYO_xX <ryo.na.na.ryo.na@gmail.com>
2026-06-05 17:09:25 +0900

    solve: ABC142-D

--- ABC/abc142/d/main.py
+++ ABC/abc142/d/main.py
@@ -1,11 +1,32 @@
 import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+##https://qiita.com/LorseKudos/items/9eb560494862c8b4eb56
+def make_divisors(n):
+    lower_divisors, upper_divisors = [], []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n // i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
 
 
 def main():
+    from math import gcd
+    from sympy import isprime
+
+    a, b = map(int, input().split())
+
+    kouyakusu = make_divisors(gcd(a, b))
+    print(sum(isprime(x) for x in kouyakusu) + 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
