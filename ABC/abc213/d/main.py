f14a788a54bc911a5996c7088cb87e3301028dcb
Xx_RYO_xX <ryo.na.na.ryo.na@gmail.com>
2026-06-11 18:17:27 +0900

    solve: ABC213-D

--- ABC/abc213/d/main.py
+++ ABC/abc213/d/main.py
@@ -1,46 +1,36 @@
-import sys 
+import sys
 
 
-def input():return sys.stdin.readline().rstrip()
-
-
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
-    from collections import defaultdict
-    from sortedcontainers import SortedList
     n = int(input())
-    g_lst = defaultdict(SortedList)
-    for _ in range(n-1):
+    g = [[] for _ in range(n + 1)]
+    for _ in range(n - 1):
         a, b = map(int, input().split())
-        g_lst[a].add(b)
-        g_lst[b].add(a)
-    print(g_lst)
-    
-    def dfs(pos, graph_lst, visited, path, goal):
-        global finished
-        if finished:
-            return
+        g[a].append(b)
+        g[b].append(a)
 
-        path.append(pos)
+    for i in range(1, n + 1):
+        g[i].sort()
+
+    visited = [True] + [False] * n
+
+    sys.setrecursionlimit(10**9)
+
+    def dfs(pos):
+        print(pos, end=" ")
         visited[pos] = True
-        if pos == goal:
-            finished = True
-            return
 
-        for i in graph_lst[pos]:
-            if not visited[i]:
-                dfs(i, graph_lst, visited, path, goal)
-                if finished:
-                    return
+        for nex in g[pos]:
+            if not visited[nex]:
+                dfs(nex)
+                print(pos, end=" ")
 
-        path.pop()
-    
-    print()
-    dfs(1, )
+    dfs(1)
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
