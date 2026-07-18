fe3a9d185c270d482b1891243e7c815c7142d69e
Xx_RYO_xX <ryo.na.na.ryo.na@gmail.com>
2026-07-18 19:05:59 +0900

    solve: ABC299-D, E

--- ABC/abc299/d/main.py
+++ ABC/abc299/d/main.py
@@ -1,17 +1,26 @@
 import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
     n = int(input())
-    
-    left = 1
-    right = n
-    while left < right:
-        
+
+    ok = 1
+    ng = n
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        print("? " + str(mid), flush=True)
+        res = int(input())
+        if res == 0:
+            ok = mid
+        else:
+            ng = mid
+
+    print("! " + str(ok), flush=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- ABC/abc299/e/main.py
+++ ABC/abc299/e/main.py
@@ -1,11 +1,57 @@
+from posixpath import sep
 import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
+    from collections import deque, defaultdict
+
+    n, m = map(int, input().split())
+    g = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        u, v = map(int, input().split())
+        g[u].append(v)
+        g[v].append(u)
+    k = int(input())
+    s = [1] * (n + 1)
+    kuro_lst = []
+    kuro = []
+    dist = defaultdict(lambda: -1)
+    q = deque()
+    for _ in range(k):
+        p, d = map(int, input().split())
+
+        kuro = []
+        dist.clear()
+        q.clear()
+        dist[p] = 0
+        q = deque([p])
+        while q:
+            pos = q.popleft()
+            if dist[pos] == d:
+                kuro.append(pos)
+                continue
+            s[pos] = 0
+            for nex in g[pos]:
+                if dist[nex] == -1:
+                    dist[nex] = dist[pos] + 1
+                    q.append(nex)
+        kuro_lst.append(kuro)
+
+    for kuro in kuro_lst:
+        for pos in kuro:
+            if s[pos] == 1:
+                break
+        else:
+            print("No")
+            return
+
+    print("Yes")
+    print(*s[1:], sep="")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
