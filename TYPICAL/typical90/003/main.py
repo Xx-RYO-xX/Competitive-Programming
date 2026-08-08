4faab41871703e182f0f591952fd1c7f61e9c1e6
Xx_RYO_xX <ryo.na.na.ryo.na@gmail.com>
2026-08-08 14:46:24 +0900

    solve: typical90-003

--- TYPICAL/typical90/003/main.py
+++ TYPICAL/typical90/003/main.py
@@ -1,11 +1,44 @@
-import sys 
+import sys
 
 
-def input():return sys.stdin.readline().rstrip()
+def input():
+    return sys.stdin.readline().rstrip()
 
 
 def main():
+    n = int(input())
+    g = [[] for _ in range(n + 1)]
+    for _ in range(n - 1):
+        a, b = map(int, input().split())
+        g[a].append(b)
+        g[b].append(a)
+
+    from collections import deque
+
+    q = deque([1])
+    dist = [-1] * (n + 1)
+    dist[1] = 0
+    while q:
+        pos = q.popleft()
+        for nex in g[pos]:
+            if dist[nex] == -1:
+                dist[nex] = dist[pos] + 1
+                q.append(nex)
+
+    hashi = dist.index(max(dist))
+
+    q = deque([hashi])
+    dist = [-1] * (n + 1)
+    dist[hashi] = 0
+    while q:
+        pos = q.popleft()
+        for nex in g[pos]:
+            if dist[nex] == -1:
+                dist[nex] = dist[pos] + 1
+                q.append(nex)
+
+    print(max(dist) + 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
